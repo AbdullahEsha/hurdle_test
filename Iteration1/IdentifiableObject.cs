682e619abf0d9f250f99810cc6c9af2559ed3373
AbdullahEsha <shahariaresha@gmail.com>
2024-03-17 15:08:57 +0600

    update data

---
+++ Iteration1/IdentifiableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iteration1;
+public class IdentifiableObject
+{
+    // Private field to store the identifiers
+    private HashSet<string> _identifiers;
+
+    // Constructor to initialize the identifiers
+    public IdentifiableObject(string[] idents)
+    {
+        _identifiers = new HashSet<string>(idents.Select(i => i.ToLower()));
+    }
+
+    // Method to check if the object has a specific identifier
+    public bool AreYou(string id)
+    {
+        return _identifiers.Contains(id.ToLower());
+    }
+
+    // Property to get the first identifier
+    public string FirstId => _identifiers.Count > 0 ? _identifiers.First() : string.Empty;
+
+    // Method to add a new identifier
+    public void AddIdentifier(string id)
+    {
+        string lowerId = id.ToLower();
+        if (!_identifiers.Contains(lowerId))
+        {
+            _identifiers.Add(lowerId);
+        }
+    }
+
+    // Method to expose the identifiers for demonstration purposes
+    public string[] GetIdentifiers()
+    {
+        return _identifiers.ToArray();
+    }
+}
