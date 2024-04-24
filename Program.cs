12a198b744b00c2c0228f4607a61d4264448f4d9
AbdullahEsha <shahariaresha@gmail.com>
2024-04-25 04:02:50 +0600

    update

--- Program.cs
+++ Program.cs
@@ -17,6 +17,13 @@ namespace HurdleTask
             sales.Add(new Transaction("10-0003", "Hunger Games 1-3", 45.00m));
             sales.Add(new Transaction("15-0020", "Learning Blender", 56.90m));
 
+            // empty batch
+            sales.Add(new Batch("2024x00003", "Empty Batch"));
+
+            // single order batch
+            sales.Add(new Batch("2024x00004", "Single Order Batch"));
+            sales.Add(new Transaction("00-0002", "The Hobbit", 12.50m));
+
             sales.PrintOrders();
         }
     }
