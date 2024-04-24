f9cc430bcece950c0d34f20aecb7e1bb3b65b958
AbdullahEsha <shahariaresha@gmail.com>
2024-04-25 03:40:07 +0600

    bug fix

--- Program.cs
+++ Program.cs
@@ -4,19 +4,6 @@ namespace HurdleTask
 {
     class Program
     {
-        // Output:
-        // Sales:
-        // Batch sale: #2024x00001, CompSci Books
-        // #1, Deep Learning in Python, $67.90
-        // #2, C# in Action, $54.10
-        // #3, Design Patterns, $129.75
-        // Total: $251.75
-        // Batch sale: #2024x00002, Fantasy Books
-        // Empty order.
-        // #00-0001, Compilers, $134.60
-        // #10-0003, Hunger Games 1-3, $45.00
-        // #15-0020, Learning Blender, $56.90
-        // Sales total: $488.25
         static void Main(string[] args)
         {
             Sales sales = new Sales();
